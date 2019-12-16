f071a1de57ed35acfb413b7aac0e1bed1ff901cd
Dr. Zeb <drzeb@machomath.com>
2019-12-16 07:20:17 -0600

    This is our first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php  ?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>Learning git</h1>
+    <pre>
+      (Before this exercise we have got git for this system already)
+      The first thing we need to do is to nitiate git in this folder.
+        go to cmd and cd to this folder and run command
+    </pre>
+
+  </body>
+</html>
